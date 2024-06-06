6d27225153c47756d135eac7d43abeacb4ad8791
Nishant9091 <nishantsharma7428@gmail.com>
2024-06-06 11:26:32 +0530

    first commit

---
+++ admin/config/database.php
@@ -0,0 +1,21 @@
+<?php
+### DATABASE CONFIGURATION
+
+// local
+// $GLOBALS['DBHOST'] = "localhost";
+// $GLOBALS['DBNAME'] = "supershop_rdb";
+// $GLOBALS['DBUSER'] = "root";
+// $GLOBALS['DBPASS'] = "";
+
+// infinity
+// $GLOBALS['DBHOST'] = "sql112.infinityfree.com";
+// $GLOBALS['DBNAME'] = "if0_36526583_shop";
+// $GLOBALS['DBUSER'] = "if0_36526583";
+// $GLOBALS['DBPASS'] = "WkRHigyhImOU";
+
+// webhost
+$GLOBALS['DBHOST'] = "localhost";
+$GLOBALS['DBNAME'] = "id22191447_supershop_rdb";
+$GLOBALS['DBUSER'] = "id22191447_root";
+$GLOBALS['DBPASS'] = "Ns999.co.in";
+?>
